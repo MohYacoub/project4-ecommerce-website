ff1292a886f9e028207309827a8507bde04b49c2
Roaaabushaqrah <roaaabushaqrah@gmail.com>
2020-12-03 23:27:21 +0300

    Update register and login file with lana and finish grid product

--- code/login.php
+++ code/login.php
@@ -1,9 +1,61 @@
+<?php
+session_start();
+include('admin/partials/connection.php');
+
+// $errorcheck = "";
+$query  = "SELECT * FROM admins";
+$result = mysqli_query($conn,$query);
+if(isset($_POST['submit'])){
+	$username = strtolower($_POST['username']);
+	$pass = $_POST['password'];
+	if(!empty($username) && !empty($pass)){
+    while($row = mysqli_fetch_assoc($result)){
+        if($username == $row["admin_name"] && $pass == $row["admin_password"] ){
+			
+			if($row['admin_role'] == 'superadmin'){
+				$_SESSION['superadmin'] = $username;
+				header("Location:index.php");
+			}
+            else {
+				$_SESSION['admin'] = $username;
+			header("Location:index.php");
+			}
+		}}
+		}
+		else {
+			$errorcheck = "Please Fill the empty field";
+		}
+		} 
+?>
+<?php
+$query2  = "SELECT * FROM customers";
+$result2 = mysqli_query($conn,$query2);
+if(isset($_POST['submit'])){
+	$username = strtolower($_POST['username']);
+	$pass = $_POST['password'];
+	if(!empty($username) && !empty($pass)){
+    while($row2 = mysqli_fetch_assoc($result2)){
+        if($username == $row2["cust_name"] && $pass== $row2["cust_password"] ){
+            $_SESSION['user'] = $username;
+            header("Location: index.php");}
+            else{
+				$errorcheck = "Incrorect Username Or Password";
+            }
+		}
+	}
+	else {
+		$errorcheck = "Please Fill the empty field";
+	}
+	}
+
+?>
+
+
+
 
 <?php
 include('partails/public_head.php');
 include('partails/public_header.php');
-
-
 ?> 
 	<div>
 	<div class="main-content main-content-login">
@@ -33,36 +85,22 @@ include('partails/public_header.php');
 								<div class="col-lg-6 col-md-6 col-sm-12">
 									<div class="login-item">
 										<h5 class="title-login">Login your Account</h5>
-										<form class="login">
-											<div class="social-account">
-												<h6 class="title-social">Login with social account</h6>
-												<a href="#" class="mxh-item facebook">
-													<i class="icon fa fa-facebook-square" aria-hidden="true"></i>
-													<span class="text">FACEBOOK</span>
-												</a>
-												<a href="#" class="mxh-item twitter">
-													<i class="icon fa fa-twitter" aria-hidden="true"></i>
-													<span class="text">TWITTER</span>
-												</a>
-											</div>
+
+										<form class="login" method="post">
+										<?php if(isset($errorcheck)){echo "<div class='alert alert-danger'> $errorcheck </div>";}?>
 											<p class="form-row form-row-wide">
 												<label class="text">Username</label>
-												<input title="username" type="text" class="input-text">
+												<input name="username"title="username" type="text" placeholder="Username" class="input-text">
 											</p>
 											<p class="form-row form-row-wide">
 												<label class="text">Password</label>
-												<input title="password" type="password" class="input-text">
-											</p>
-											<p class="lost_password">
-												<span class="inline">
-													<input type="checkbox" id="cb1">
-													<label for="cb1" class="label-text">Remember me</label>
-												</span>
-												<a href="#" class="forgot-pw">Forgot password?</a>
+												<input name="password" title="password" type="password" placeholder="Password" class="input-text">
 											</p>
 											<p class="form-row">
-												<input type="submit" class="button-submit" value="login">
+												<input name="submit" type="submit" class="button-submit" value="login">
+
 											</p>
+											
 										</form>
 									</div>
 								</div>
--- code/register.php
+++ code/register.php
@@ -35,40 +35,49 @@ $conn = mysqli_connect("localhost","root","","kidsstoredb");
         
 if(isset($_POST["submit"])){
 
-$name=$_POST['username'];
-$email=$_POST['email'];
- $password=$_POST['pass'];
+$name  = strtolower($_POST['username']);
+$email = strtolower($_POST['email']);
+ $password= $_POST['pass'];
 //  $pwd= md5($password);
- $cpassword=$_POST['cpass'];
- $phone=$_POST['phone'];
- $address=$_POST['address'];
+ $cpassword = $_POST['cpass'];
+ $phone= $_POST['phone'];
+ $address= $_POST['address'];
 
- if(!empty($_POST['username']) && !empty($_POST['pass'])&& !empty($_POST['email'])){
-
-    if(($_POST['pass'])==($_POST['cpass'])){
-
-        $sel="SELECT *FROM  customers WHERE cust_name ='$name' ||  cust_email ='$email'" ;
-                                 
-        $num= mysqli_query($conn,$sel); 
-        $row=mysqli_fetch_assoc($num); 
-        if(!$row){ $query = "INSERT INTO customers (cust_name,cust_password,cust_email,cust_phone,cust_address)
-            VALUES ('$name','$password','$email','$phone',' $address')";
-            mysqli_query($conn,$query);
-            
-
-        } else {
-           $err= "uesrname  or email is alerady exist";
-        }
-
-    } else {
-        $err=" password not matching";
-    } 
-
-
-   
-    
-} else{
-    $err= " please";
+ if (!empty($name) && !empty($_POST['pass']) && !empty($email)) {
+	if ($password == $cpassword) {
+		$check_user = "SELECT *FROM  customers WHERE cust_name ='$name' ||  cust_email ='$email' || cust_phone ='$phone'";
+		$num = mysqli_query($conn, $check_user);
+		$row = mysqli_fetch_assoc($num);
+		if (!$row) {
+			$query = "INSERT INTO customers (cust_name,cust_password,cust_email,cust_phone,cust_address)
+		VALUES ('$name','$password','$email','$phone',' $address')";
+			mysqli_query($conn, $query);
+		} 
+		elseif (($email == $row['cust_email']) && ($name == $row['cust_name']) && ($phone == $row['cust_phone'])) {
+			$err = "email, uesrname and phone is alerady exist";
+		}
+		elseif (($email == $row['cust_email']) && ($name == $row['cust_name'])) {
+			$err = "email and uesrname is alerady exist";
+		}
+		elseif (($email == $row['cust_email']) && ($phone == $row['cust_phone'])) {
+			$err = "email and phone is alerady exist";
+		}
+		elseif (($name == $row['cust_name']) && ($phone == $row['cust_phone'])) {
+			$err = "uesrname and phone is alerady exist";
+		}
+		 elseif ($name == $row['cust_name']) {
+			$err = "uesrname is alerady exist";
+		} elseif ($email == $row['cust_email']) {
+			$err = "email is alerady exist";
+		}
+		elseif($phone == $row['cust_phone']){
+			$err = "phone is alerady exist";
+		}
+	} else {
+		$err = " password not matching";
+	}
+} else {
+	$err = " please fill the empty fields";
 }
     }
 ?>
